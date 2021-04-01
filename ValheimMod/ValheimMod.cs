7c767dc7a7bb26deb80bc514735868b7b4152c8c
MisterShake2099 <mistershake2099@gmail.com>
2021-04-01 09:34:56 -0700

    Initial commit.

---
+++ ValheimMod/ValheimMod.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ValheimMod
+{
+    [BepInPlugin("mistershake2099.ValheimMod", "Valheim Mod", "0.0.1")]
+    [BepInProcess("valheim.exe")]
+
+    public class ValheimMod : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("mistershake2099.ValheimMod");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Character), nameof(Character.Jump))]
+        class Jump_Patch
+        {
+            static void Prefix(ref float ___m_jumpForce)
+            {
+                Debug.Log($"Jump force: {___m_jumpForce}");
+                ___m_jumpForce = 15;
+                Debug.Log($"Modified jump force: {___m_jumpForce}");
+            }
+        }
+    }
+}
